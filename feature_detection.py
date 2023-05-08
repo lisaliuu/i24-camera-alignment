0a652a5c692fc1fde57bc5d9e5332636d4bef95c
lisaliuu <82255401+lisaliuu@users.noreply.github.com>
2023-05-08 14:26:47 -0500

    commented

--- feature_detection.py
+++ feature_detection.py
@@ -37,12 +37,14 @@ home = {1: {'p':-152.06, 't':-22.6},
         6: {'p':-71.45, 't':-26.29}}
 
 def format_to_csv(filename, num_points_matched, M_rot, E_rot1, E_rot2 ):
+    '''
+    Save data to CSV
+    '''
     data = []
     name_split = filename.split('_')
     cam_num = int(name_split[0][-1:]) #last number of ip address
     abs_pan = float(name_split[1][1:])
     abs_tilt = float(name_split[2][1:][:-4])
-    # print('home[cam_num] ', home[cam_num])
     rel_pan = home[cam_num]['p']-abs_pan
     rel_tilt = home[cam_num]['t']-abs_tilt
     data.append(cam_num)
@@ -59,7 +61,6 @@ def format_to_csv(filename, num_points_matched, M_rot, E_rot1, E_rot2 ):
     E_degrees1 = rot_matrix_to_degrees(E_rot1)
     E_degrees2 = rot_matrix_to_degrees(E_rot2)
 
-    # print(M_degrees)
     M_degrees=np.around(M_degrees, decimals=1)
     E_degrees1=np.around(E_degrees1, decimals=1)
     E_degrees2=np.around(E_degrees2, decimals=1)
@@ -68,19 +69,17 @@ def format_to_csv(filename, num_points_matched, M_rot, E_rot1, E_rot2 ):
         M_degrees2=M_degrees[2]
     else:
         M_degrees2=None
-    # print(M_degrees1)
-    # print(M_degrees2)
-    # exit()
     data.append(M_degrees1)
     data.append(M_degrees2)
     data.append(E_degrees1)
     data.append(E_degrees2)
-    
-    # print(data)
 
     return data
 
 def load_images_from_folder(folder):
+    '''
+    Load all query images from folder
+    '''
     images=[]
     count=0
     for filename in os.listdir(folder):
@@ -91,6 +90,9 @@ def load_images_from_folder(folder):
     return images
 
 def read_images(filename, ref_file_name, img_file_name):
+    '''
+    Reads and returns reference image and query image to align
+    '''
      # Training image
     print(ALGO+": Reading reference image: ", filename+ref_file_name)
     ref_img = cv.imread(filename+ref_file_name, cv.COLOR_BGR2GRAY)
@@ -102,6 +104,9 @@ def read_images(filename, ref_file_name, img_file_name):
     return ref_img, img
 
 def find_good_matches(matches, kp_img, kp_rimg, ratio, match_count):
+    '''
+    Returns points of good matches
+    '''
     # Store all the good matches as per Lowe's ratio test
     good = []
     for m,n in matches:
@@ -118,7 +123,11 @@ def find_good_matches(matches, kp_img, kp_rimg, ratio, match_count):
    
     return dst_pts, src_pts, good
 
+
 def rot_matrix_to_degrees(rot):
+    '''
+    Converts rotation matrix to rotation array in degrees
+    '''
     euler_angles = rotationMatrixToEulerAngles(rot)
     # print(str(i)+" eurler angles: ", euler_angles)
     degrees_angles = euler_angles
@@ -127,6 +136,9 @@ def rot_matrix_to_degrees(rot):
     return degrees_angles
 
 def draw_and_save_lines(M, mask, img, img_reference, kp_img, kp_rimg, good, out_file_postfix):
+    '''
+    Writes an image with matches drawn between reference image and query image
+    '''
     matchesMask = mask.ravel().tolist()
     h,w = img.shape[:-1]
     pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
@@ -148,6 +160,9 @@ def draw_and_save_lines(M, mask, img, img_reference, kp_img, kp_rimg, good, out_
     cv.imwrite(out_file_name, img3)
 
 def save_planar_transform(M, img, img_reference, out_file_postfix):
+    '''
+    Writes warped query image after applying homography
+    '''
     # Use homography
     height, width, channels = img_reference.shape
     img_aligned = cv.warpPerspective(img, M, (width, height))
@@ -158,6 +173,9 @@ def save_planar_transform(M, img, img_reference, out_file_postfix):
     cv.imwrite(out_file_name, img_aligned)
 
 def print_homography_result(M, rot, tran):
+    '''
+    Prints homography and calculated rotation array
+    '''
     print("homography: \n", M)
     print("\n")
     for i in range(4):
@@ -168,6 +186,9 @@ def print_homography_result(M, rot, tran):
             # print("translation "+str(i)+": ", tran[i])
 
 def print_essential_result(E, rot, tran):
+    '''
+    Prints essential and calculated rotation array
+    '''
     print("Essential Matrix: \n", E)
     print("\n")
     degrees_angles = rot_matrix_to_degrees(rot)
@@ -175,6 +196,9 @@ def print_essential_result(E, rot, tran):
     # print("translation: ", tran[0])
 
 def sift_algo(filename, ref_file_name, img_file_name, out_file_postfix):
+    '''
+    Runs SIFT feature detection algorithm
+    '''
     # Read in training and query image
     ref_img, img = read_images(filename, ref_file_name, img_file_name)
     
@@ -216,6 +240,9 @@ def sift_algo(filename, ref_file_name, img_file_name, out_file_postfix):
 
 
 def brisk_algo(filename, ref_file_name, img_file_name, out_file_postfix):
+    '''
+    Runs BRISK feature detection algorithm
+    '''
     # Read in training and query image
     ref_img, img = read_images(filename, ref_file_name, img_file_name)
     
@@ -251,7 +278,7 @@ def brisk_algo(filename, ref_file_name, img_file_name, out_file_postfix):
 
 
     # Saves image of matches drawn and image that has been aligned with transformation
-    # draw_and_save_lines(M, mask, img, ref_img, kp_img, kp_rimg, good, out_file_postfix)
+    draw_and_save_lines(M, mask, img, ref_img, kp_img, kp_rimg, good, out_file_postfix)
     # draw_and_save_lines(E, mask, img, ref_img, kp_img, kp_rimg, good, out_file_postfix)
 
     # save_planar_transform(M, img, ref_img, out_file_postfix)
@@ -260,6 +287,9 @@ def brisk_algo(filename, ref_file_name, img_file_name, out_file_postfix):
 
 
 def orb_algo(filename, ref_file_name = "ref_image.jpg", img_file_name = "image.jpg", out_file_postfix=""):
+    '''
+    Runs ORB feature detection algorithm
+    '''
     # Read in training and query image
     ref_img, img = read_images(filename, ref_file_name, img_file_name)
     
@@ -303,47 +333,12 @@ def orb_algo(filename, ref_file_name = "ref_image.jpg", img_file_name = "image.j
 
     return M_rot, E_rot1, E_rot2, len(good)
 
-def surf_algo(filename, ref_file_name = "ref_image.jpg", img_file_name = "image.jpg", out_file_postfix=""):
-    # Read in training and query image
-    ref_img, img = read_images(filename, ref_file_name, img_file_name)
-
-    # Detect ORB features and compute decriptors
-    surf = cv.SURF_create()
-
-    # Find the keypoints and descriptors with ORB
-    kp_rimg, descr_rimg = surf.detectAndCompute(ref_img, None)
-    kp_img, descr_img = surf.detectAndCompute(img, None)
-
-    # Match features with brute force
-    matcher = cv.BFMatcher(cv.NORM_L2)
-    matches = matcher.matcher.knnMatch(descr_rimg, trainDescriptors = descr_img, k = 2) #2
-
-    # Returns list of "good" matches 
-    dst_pts, src_pts, good = find_good_matches(matches, kp_img, kp_rimg, SURF_LOWE_DIST_RATIO, 0)
-
-    # Calculate essential matrix
-    E, mask = cv.findEssentialMat(points1=dst_pts, points2=src_pts, focal=FOCAL_LEN, pp=PP)
-    e_points, E_rot, E_tran, mask = cv.recoverPose(E, points1=dst_pts, points2=src_pts)
-    # print_essential_result(E, E_rot, E_tran)
-
-    # Find homography
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC) # srcPoints, dstPoints
-    M_ret, M_rot, M_tran, M_norm = cv.decomposeHomographyMat(M, CAM_INTR)
-    # print_homography_result(M, M_rot, M_tran)
-
-    draw_and_save_lines(M, mask, img, ref_img, kp_img, kp_rimg, good, out_file_postfix)
-    # save_planar_transform(M, img, ref_img, out_file_postfix)
-
-    return M_rot, E_rot
 
 def main():
     # Setting up
-    # test='10.80.134.61_p-162.20_t-22.60.jpg'
-    # print(test[11:][:-4])
-    # exit()
     global ALGO
     ref_filename = "home.jpg"
-    foldername= "cam6"
+    foldername= "cam1temp2"
     image_names=load_images_from_folder(foldername)
     out_file_postfix = ""
 
@@ -382,12 +377,12 @@ def main():
                 csv_data = format_to_csv(image_name, num_good, M_rot, E_rot1, E_rot2 )
                 writer.writerow(csv_data)
 
-        # elif (ALGO=="orb"):
-        #     print("Starting ORB algorithm...")
-        #     for image_name in image_names:
-        #         M_rot, E_rot1, E_rot2, num_good = orb_algo(filename=f'{foldername}/', ref_file_name=ref_filename, img_file_name=image_name, out_file_postfix=image_name[11:][:-4])
-                # csv_data = format_to_csv(image_name, M_rot, E_rot)
-                # writer.writerow(csv_data)
+        elif (ALGO=="orb"):
+            print("Starting ORB algorithm...")
+            for image_name in image_names:
+                M_rot, E_rot1, E_rot2, num_good = orb_algo(filename=f'{foldername}/', ref_file_name=ref_filename, img_file_name=image_name, out_file_postfix=image_name[11:][:-4])
+                csv_data = format_to_csv(image_name, num_good, M_rot, E_rot1, E_rot2 )
+                writer.writerow(csv_data)
 
         elif (ALGO=="brisk"):
             print("Starting BRISK algorithm...")
@@ -403,6 +398,4 @@ def main():
         print(f'{ALGO} time taken: {end-start}')
 
 if __name__ == '__main__':
-    # print(cv.__version__)
-    # exit()
     sys.exit(main())
